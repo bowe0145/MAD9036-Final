997f6b2306a3741baad231bee6e13d6a337ee473
bowe0145 <bowe0145@algonquinlive.com>
2017-12-10 11:27:06 -0500

    Initial Commit

---
+++ MAD9036 Final/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAD9036 Final
+//
+//  Created by Ryan Bowes on 2017-12-10.
+//  Copyright © 2017 Algonquin College. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
