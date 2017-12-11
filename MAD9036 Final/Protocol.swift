87dfc38d3032ace83422e367a6c200be214eed98
bowe0145 <bowe0145@algonquinlive.com>
2017-12-10 20:35:50 -0500

    Created to send buildings to main thread

---
+++ MAD9036 Final/Protocol.swift
@@ -0,0 +1,13 @@
+//
+//  Protocol.swift
+//  MAD9036 Final
+//
+//  Created by Ryan Bowes on 2017-12-10.
+//  Copyright © 2017 Algonquin College. All rights reserved.
+//
+
+import Foundation
+
+protocol sendBuildings {
+    func sendBuildings(buildings: [Building]) // Protocol definition that lets a building array be passed in as a parameter
+}
