52f75a851dd556af87662ad2aad1f0ba7f19d291
bowe0145 <bowe0145@algonquinlive.com>
2017-12-10 20:35:10 -0500

    Created error catching Added delegate to send buildings to main thread

---
+++ MAD9036 Final/DoorsOpen.swift
@@ -0,0 +1,66 @@
+//
+//  DoorsOpen.swift
+//  MAD9036 Final
+//
+//  Created by Ryan Bowes on 2017-12-10.
+//  Copyright © 2017 Algonquin College. All rights reserved.
+//
+
+import Foundation
+
+class DoorsOpen {
+    var ApiUrl: URL = URL(string: "https://doors-open-ottawa.mybluemix.net/buildings")!
+    var Buildings: [Building]? = []
+    var delegate: sendBuildings?
+    
+    init () {
+        loadBuildings()
+    }
+    
+    func loadBuildings() {
+        
+        // Create the request object and pass in your url
+        let myRequest: URLRequest = URLRequest(url: ApiUrl)
+        
+        // Create the URLSession object that will make the request
+        let session: URLSession = URLSession.shared
+        
+        // The data task retrieves the data.
+        let dataTask = session.dataTask(with: myRequest) {
+            (data, response, error) -> Void in
+            
+            guard error == nil, let data = data else {
+                print("ERROR")
+                return
+            }
+            
+            self.loadBuildingsCallback(data: data)
+        }
+        dataTask.resume()
+    }
+    
+    func loadBuildingsCallback (data: Data) {
+        do {
+            if let buildings = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [[String:Any]] {
+                for building in buildings {
+                    let tempBuilding: Building = Building(building: building)
+                    self.Buildings?.append(tempBuilding)
+                }
+            }
+        } catch let jsonError {
+            // An error occurred while trying to convert the data into a Swift dictionary.
+            print("JSON error description: \(jsonError)")
+        }
+        
+        // This callback is run on a secondary thread, so you must make any UI changes on the main thread by calling the DispatchQueue.main.async() method
+        DispatchQueue.main.async() {
+
+            guard let theBuildings = self.Buildings, self.Buildings?.count != nil else {
+                return
+            }
+            self.delegate?.sendBuildings(buildings: theBuildings)
+        }
+        
+    }
+    
+}
