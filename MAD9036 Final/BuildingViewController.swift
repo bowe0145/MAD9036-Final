e13bb85ac8c4dacad7f3471646132ff27b325e79
bowe0145 <bowe0145@algonquinlive.com>
2017-12-13 21:17:41 -0500

    Created Controller for viewing a building

---
+++ MAD9036 Final/BuildingViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BuildingViewController.swift
+//  MAD9036 Final
+//
+//  Created by Ryan Bowes on 2017-12-13.
+//  Copyright © 2017 Algonquin College. All rights reserved.
+//
+
+import UIKit
+
+class BuildingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
