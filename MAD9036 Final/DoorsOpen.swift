53459db6196ee25f588ed04a4bb33f3718d00889
bowe0145 <bowe0145@algonquinlive.com>
2017-12-13 21:11:59 -0500

    Added a callback with 2 parameters (Data, Any) Cleaned up the code Added a default URL

--- MAD9036 Final/DoorsOpen.swift
+++ MAD9036 Final/DoorsOpen.swift
@@ -9,55 +9,40 @@
 import Foundation
 
 class DoorsOpen {
-    var ApiUrl: URL = URL(string: "https://doors-open-ottawa.mybluemix.net/buildings")!
+    var ApiUrl: String =  "https://doors-open-ottawa.mybluemix.net/"
     var Buildings: [Building]? = []
-    var delegate: sendBuildings?
+    var instance: DoorsOpen!
     
     init () {
-        loadBuildings()
+        instance = self
     }
     
-    
-    
-    func loadBuildings() {
+    func prepareRequest( URLString: String, extra: Any?, completion: ((Data, Any?) -> Void)? ) {
+        let fullString = ApiUrl + URLString
+        let tempUrl = fullString.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed)
+        let url: URL! = URL(string: tempUrl!)
         
-        // Create the request object and pass in your url
-        let myRequest: URLRequest = URLRequest(url: ApiUrl)
+        let request: URLRequest = URLRequest(url: url)
         
-        // Create the URLSession object that will make the request
         let session: URLSession = URLSession.shared
         
-        // The data task retrieves the data.
-        let dataTask = session.dataTask(with: myRequest) {
-            (data, response, error) -> Void in
-            
-            guard error == nil, let data = data else {
-                print("ERROR")
-                return
-            }
-            
-            do {
-                if let buildings = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [[String:Any]] {
-                    for building in buildings {
-                        let tempBuilding: Building = Building(building: building)
-                        self.Buildings?.append(tempBuilding)
-                    }
-                }
-            } catch let jsonError {
-                // An error occurred while trying to convert the data into a Swift dictionary.
-                print("JSON error description: \(jsonError)")
-            }
-            
-            // This callback is run on a secondary thread, so you must make any UI changes on the main thread by calling the DispatchQueue.main.async() method
-            DispatchQueue.main.async() {
-                
-                guard let theBuildings = self.Buildings, self.Buildings?.count != nil else {
+        // Make the specific task from the session by passing in your request
+        let task = session.dataTask(with: request) { (responseData, response, responseError) in
+            DispatchQueue.main.async {
+                guard responseError == nil else {
                     return
                 }
-                self.delegate?.sendBuildings(buildings: theBuildings)
+                
+                guard let jsonData = responseData else {
+                    _ = NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey : "Data was not retrieved from request"]) as Error
+                    return
+                }
+                
+                completion?(jsonData, extra)
             }
         }
-        dataTask.resume()
+        
+        // Tell the task to run
+        task.resume()
     }
-    
 }
